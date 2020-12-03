91f4db944fed12beacdda7a84d04d64c7f671561
tanghu1999 <1621037507@qq.com>
2020-12-03 21:34:11 +0800

    first modify

--- trip-manager/trip-manager-utils/src/main/java/com/th/utils/SMSUtils.java
+++ trip-manager/trip-manager-utils/src/main/java/com/th/utils/SMSUtils.java
@@ -35,8 +35,8 @@ public class SMSUtils {
         final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
         // 替换成你的accessKeyId 和 accessKeySecret
-        final String accessKeyId = "你的accessKeyId";// 你的accessKeyId,参考准备阶段第8步
-        final String accessKeySecret = "你的accessKeySecret";// 你的accessKeySecret，参考准备阶段第8步
+        final String accessKeyId = "";// 你的accessKeyId,参考准备阶段第8步
+        final String accessKeySecret = "";// 你的accessKeySecret，参考准备阶段第8步
         // 初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
